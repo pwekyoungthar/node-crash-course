282e49f6fc9f9a5cb523b1b62bcd0b1d204b383f
Thu Yein Soe <pwekyoungthar@gmail.com>
2023-06-27 10:14:51 +0630

    Node Tuto 2 Finish

---
+++ 01Node/math.js
@@ -0,0 +1,11 @@
+exports.add = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divided = (a, b) => a / b;
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divided,
+};
---
+++ 01Node/server.js
@@ -0,0 +1,8 @@
+const os = require("os");
+const path = require("path");
+const { add, subtract, multiply, divided } = require("./math");
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divided(2, 3));
